67230ae92ec5ad4b47942c9ffddb39f3ff85f3ec
Liuxl945 <290698200@qq.com>
2021-06-12 23:06:58 +0800

    切换直播源   生成数据

---
+++ getdata/mu3tojson.js
@@ -0,0 +1,51 @@
+const fs = require("fs")
+
+let data = fs.readFileSync("./国内地方台202104.m3u")
+let oldJson = JSON.parse(fs.readFileSync("./live.json").toString())
+
+
+let arr = data.toString().split(/\n/)
+
+let newArr = []
+
+for (let i = 0; i < arr.length - 1; i++) {
+    const element = arr[i];
+    if(element.includes("#EXTINF:-1")) {
+        newArr.push({
+            channelName: element.replace("#EXTINF:-1 ,", "").trim(),
+            channelUrls: arr[i + 1].trim()
+        })
+    }
+}
+
+let jsonArr = []
+
+newArr.forEach(item => {
+    let find = jsonArr.filter(i => {
+        return i.channelName === item.channelName
+    })[0]
+
+    // let isNeed = oldJson.filter(i => {
+    //     return i.channelName === item.channelName
+    // })
+
+    if(find === undefined) {
+        jsonArr.push({
+            channelName: item.channelName,
+            channelUrls: [item.channelUrls]
+        })
+    }else {
+        find.channelUrls.push(item.channelUrls)
+    }
+    
+})
+
+jsonArr.sort((a,b) => {
+    return a.channelName - b.channelName
+})
+
+
+fs.writeFileSync("mu3.json", JSON.stringify(jsonArr,"","\t"))
+
+
+console.log(jsonArr.length)
